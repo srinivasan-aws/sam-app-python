ed7116a089fed27eaf2d2a6a33658c61162fe431
srinivasan-aws <sbkshns@gmail.com>
2024-11-14 18:26:57 +0530

    Changed return message

--- hello-world/tests/unit/test-handler.js
+++ hello-world/tests/unit/test-handler.js
@@ -6,16 +6,16 @@ const expect = chai.expect;
 var event, context;
 
 describe("Tests index", function () {
-  it("verifies successful response", async () => {
-    const result = await app.lambdaHandler(event, context)
-
-    expect(result).to.be.an("object")
-    expect(result.statusCode).to.equal(200)
-    expect(result.body).to.be.an("string")
-
-    let response = JSON.parse(result.body)
-
-    expect(response).to.be.an("object")
-    expect(response.message).to.be.equal("I'm using canary deployments")
+    it("verifies successful response", async () => {
+      const result = await app.lambdaHandler(event, context)
+  
+      expect(result).to.be.an("object")
+      expect(result.statusCode).to.equal(200)
+      expect(result.body).to.be.an("string")
+  
+      let response = JSON.parse(result.body)
+  
+      expect(response).to.be.an("object")
+      expect(response.message).to.be.equal("Welcome to hello world")
+    })
   })
-})

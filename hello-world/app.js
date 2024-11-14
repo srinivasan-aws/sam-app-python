6de2bcf82c0ed698eaa65ce0d9f5372979f6ed3a
srinivasan-aws <sbkshns@gmail.com>
2024-11-14 18:25:15 +0530

    Add Canary deployment configuration to SAM

--- hello-world/app.js
+++ hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             statusCode: 200,
             body: JSON.stringify({
-              message: "I'm using canary deployments",
+              message: "Welcome to hello world",
             }),
           }
     } catch (err) {

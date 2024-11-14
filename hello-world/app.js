1ca2d9b48ec283b3df704663fe0757c4d7cc4cd4
srinivasan-aws <sbkshns@gmail.com>
2024-11-14 16:08:49 +0530

    update code

--- hello-world/app.js
+++ hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello hello my friend',
+                message: 'hello hello my dear friend',
                 // location: ret.data.trim()
             })
         }
